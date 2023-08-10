5c7714ae0c8dcd3b0cc02284667fbfc2f383057e
pc200516153 <133276045+pc200516153@users.noreply.github.com>
2023-08-10 16:11:52 -0400

    Update systemreport.ps1

--- systemreport.ps1
+++ systemreport.ps1
@@ -1,22 +1,25 @@
+#this is the function to get hardware information of the system
 function SystemHardwareInfo{
 $hardwaredescription= Get-WmiObject Win32_computersystem
 Write-Host "Hardware description"
 $hardwaredescription
 }
 
+#This is the function to get operating system information 
 function OperatingSystemInfo{
 $operatingsystem = Get-WmiObject Win32_operatingsystem
-Write-Host "operating system"
+Write-Host "Operating system"
 $operatingsystemname= $operatingsystem.caption
 Write-Host "Operatingsystem Name: $operatingsystemname"
 $operatingsystemversion = $operatingsystem.version
 Write-Host "Operatingsystem Version:$operatingsystemversion"
 }
 
+#This is to create space 
 Write-Host
 
 
-
+#This is the function to get the processor information
 function ProcessorInormation{
 $processordescription=Get-WmiObject Win32_processor
 Write-Host "Processor Information"
@@ -35,8 +38,10 @@ $L3CacheSize = $processordecription.L3Cachesize
 Write-Host "L3 Cache Size : $L3cachesize"
 }
 
+#This is to create space
 Write-Host
 
+#This is the function to get Ram information of the system
 function RamInformation{
 Write-Host "RAM Information"
 $totalcapacity = 0
@@ -56,6 +61,7 @@ ft -auto Manufacturer, "Size(MB)",Description,"Speed(MHz)",Bank,Slot
 "Total RAM Installed: ${totalcapacity}MB"
 }
 
+#This is the function to get Disk Information of the system
 function DiskInformation{
 Write-Host "Physical Disk Drivers Summary"
 $diskdrives = Get-CIMInstance CIM_diskdrive
@@ -75,7 +81,7 @@ $diskdrives = Get-CIMInstance CIM_diskdrive
   }
 }
 
-
+#This is the function to get network configuration of the system
 function NetworkConfiguration{
 Write-Host "Network Adapter Configuration"
 $adapters = Get-WmiObject Win32_NetworkAdapter
@@ -83,7 +89,7 @@ $filteredadapters = $adapters | where-object {$_.netenabled -eq $true}
 $filteredadapters | format-table
 }
 
-
+#This is the function to get Video Card Information of the system
 function VideoCardInformation{
 Write-Host "video Card Information"
 $VideoCardInfo = Get-WmiObject win32_videocontroller
